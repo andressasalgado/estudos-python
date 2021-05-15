84a35e8f468f3bde261e64789f32d6d140646333
gitandressa <andressa.salgado@outlook.com>
2021-05-15 02:25:33 -0300

    start w/ concatenation

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# # String concatenation
+# I see two ways to do this
+# print("Hello, " + World)
+# print(f"Hello, {World}")
+
+world = "World"
+print("Hello, " + world)
+print(f"Hello, {world}")
+
+your_name = input("Hi! What's your name? ")
+print("Nice to meet you, " + your_name)
