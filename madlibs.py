6e0a9ee4c8ebe8a22f598238f8f4eb0c8da7025d
gitandressa <andressa.salgado@outlook.com>
2021-05-15 21:52:31 -0300

    madlibs game

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,50 @@
 # # String concatenation
 # I see two ways to do this
+# world = "World"
 # print("Hello, " + World)
 # print(f"Hello, {World}")
 
-world = "World"
-print("Hello, " + world)
-print(f"Hello, {world}")
-
 your_name = input("Hi! What's your name? ")
 print("Nice to meet you, " + your_name)
+print("Please, set  words to complete the song!")
+
+one_piece_hero = input("Set a One Piece hero: ")
+direction = input("Set a direction: ")
+mugiwara = input("Set a mugiwara: ")
+mugiwara2 = input("Set a mugiwara: ")
+season = input("Set a season: ")
+adj2 = input("Set a adjective: ")
+one_piece_villain = input("Set a One Piece villain: ")
+one_piece_villain2 = input("Set a One Piece villain: ")
+adj3 = input("Set a adjective: ")
+noun4 = input("Set a noun: ")
+noun5 = input("Set a noun: ")
+adj4 = input("Set a adjective: ")
+adj5 = input("Set a adjective: ")
+pirate_gang = input("Set a pirate gang: ")
+adj6 = input("Set a adjective: ")
+phrase = input("Set a phrase: ")
+phrase2 = input("Set a phrase: ")
+
+
+music = print(f"\nHere comes the {one_piece_hero} do, do, do \n"
+              f"Here comes the {one_piece_villain} \n"
+              f"And I say it's all {direction} \n"
+              f"Little {mugiwara}, it's been a long cold lonely {season} \n"
+              f"Little {adj2}, it seems like years since it's been here \n"
+              f"Here comes the {one_piece_villain2} do, do, do \n"
+              f"Here comes the {mugiwara2} \n"
+              f"And I say it's all right \n"
+              f"Little {adj3}, the smiles returning to the {noun4} \n"
+              f"Little darling, it feels like {noun5} since it's been here \n"
+              f"Here comes the sun do, do, do \n"
+              f"Here comes the sun \n"
+              f"And I say it's all right \n"
+              f"{adj4} darling, I feel that {pirate_gang} is slowly {adj5} \n"
+              f"Little darling, {one_piece_hero} seems like years since it's been {adj6} \n"
+              f"Here comes the sun do, do, do \n"
+              f"Here comes the sun \n"
+              f"And I say {phrase} \n"
+              f"Here comes the sun do, do, do \n"
+              f"Here comes the sun \n"
+              f"And I say {phrase2}")
